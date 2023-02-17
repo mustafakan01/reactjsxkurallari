c53f926b3e356ba011bd57ab65d8077decf881fc
Mustafa Kan <mustafakan242@gmail.com>
2023-02-17 22:27:51 +0300

    proje

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const nameSurname="Halit Mustafa Kan";
+  const studenst=43000;
+  return (   
+    <>
+    {/* <div>
+     Ad Soyad: {nameSurname}
     </div>
+    <div>
+        Mustafa
+      </div>
+      <input type="date"/>
+      <p>Toplam öğrenci sayısı: {studenst}</p>
+      <div>
+        <h1>Shopping List for</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+      <img src="https://picsum.photos/id/1/200/300" class="" alt=""/>
+      <div>
+        <h2>demene</h2>
+        </div> */}
+        <div className="box">
+
+        </div>
+        <br></br>
+        <div style={{
+          width:'250px',
+          height:'250px',
+          backgroundColor:'blue'}}></div>
+      </>
+
+  
   );
 }
 
